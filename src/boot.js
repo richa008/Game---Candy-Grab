1bdd85b05b77124c4a7ccb4f88a1490346d23332
Deshmukh,Richa <Richa.Deshmukh@Cerner.com>
2016-03-29 16:24:27 -0500

    Adding game set up files

---
+++ src/boot.js
@@ -0,0 +1,18 @@
+var Candy = {};
+
+Candy.boot = function(game){};
+
+Candy.boot.prototype = {
+    //Preload and create are reserved names for functions in Phaser. create() is called after preload()
+    preload: function(){
+        this.load.image('preloaderBar', 'img/loading-bar.png');
+    },
+    create: function(){
+        this.input.maxPointers = 1; //We don't use multi touch
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; // Game fits to given dimensions
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+        this.scale.setScreenSize(true); //allows scaling
+        this.state.start('Preloader'); //Executes preloader 
+    }
+}
